b6412c9d114d4962c4082ca37b0370b18aef7a5d
RonenAlbagli <AlbagliRonen@gmail.com>
2017-10-13 21:03:49 +0300

    +recipeApp

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App'
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'
+
+const store =createStore(rootReducer);
+// sto
+store.subscribe(()=> console.log('store', store.getState()));
+
+ReactDOM.render( 
+    <Provider store={store}>
+        <App/>
+
+    </Provider>
+    , document.getElementById('root'));  
+
